4aea55b36db8288c3c436c78423c6bac5bedcfea
apoorva batham <apba00001@uni-saarland.de>
2024-05-17 17:49:59 +0200

    +task 4 full

--- src/convolution.c
+++ src/convolution.c
@@ -13,6 +13,19 @@ void convolve(float *result, const float *img, int w, int h,
     (void)matrix;
     (void)w_m;
     (void)h_m;
+    int a= w_m/2;
+    int b= h_m/2;
+for (int x=0; x<w ;x++ ) {
+    for(int y=0; y<h; y++){
+        float ans=0;
+        for(int i=0; i<w_m; i++){
+            for(int j=0; j<h_m; j++){
+                ans+= matrix[j* w_m+1] *get_pixel_value(img, w, h, x-a+i,y-b+j);
+            };
+        };
+        result[y*w+x]=ans;
+    };
 
+};
     // TODO: Implement me!
 }
